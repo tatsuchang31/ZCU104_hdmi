b0b6d6c58cb97932976b0d1a8e88c348827249af
tatsuchang31 <82640723+tatsuchang31@users.noreply.github.com>
2022-08-05 12:55:57 +0900

    Update type_pack.vhd

--- type_pack.vhd
+++ type_pack.vhd
@@ -7,7 +7,7 @@ constant RAM_DATALENGTH	: INTEGER := 48;
 constant RAM_ADDRESS	: INTEGER := 16384;
 constant RAM_ADDRESSBIT	: INTEGER := 14;
 
-constant H_POS	: INTEGER := 1920;
-constant V_POS	: INTEGER := 1080;
+constant H_POS	: INTEGER := 1920; --4K x size(HDMI IP sends two pixels per clock, so half size)
+constant V_POS	: INTEGER := 2160; --4K y size
 
 end package type_pack;
